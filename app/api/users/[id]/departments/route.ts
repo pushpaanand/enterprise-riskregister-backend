889e8b097e91445cac57d8e2b030bddfc48f8ab6
pushpaanand <productananlyst.pushpa@kauveryhospital.com>
2026-01-02 15:59:45 +0530

    build error fixed for lib path

--- app/api/users/[id]/departments/route.ts
+++ app/api/users/[id]/departments/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getPool } from '../../../../lib/db';
+import { getPool } from '@/lib/db';
 import sql from 'mssql';
 
 export const runtime = 'nodejs';
